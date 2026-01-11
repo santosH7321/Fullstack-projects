a3443fd767bcb07fb9ba2fcc17f4dd2ee0d576e1
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-01-11 16:59:44 +0530

    Google oauth login

--- book-selling-platform/frontend/src/pages/Login.jsx
+++ book-selling-platform/frontend/src/pages/Login.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { GoogleLogin } from "@react-oauth/google"
+
 import api from "../services/api"
 import { useAuth } from "../context/AuthContext"
-import { useNavigate } from "react-router-dom"
 
 export default function Login() {
   const [form, setForm] = useState({ email: "", password: "" })
@@ -15,9 +17,19 @@ export default function Login() {
     navigate("/")
   }
 
+
+  const handleGoogleLogin = async (credentialResponse) => {
+    const res = await api.post("/auth/google", {
+      token: credentialResponse.credential,
+    })
+
+    login(res.data)
+    navigate("/")
+  }
+
   return (
-    <div className="max-w-md mx-auto mt-10">
-      <h2 className="text-2xl font-bold mb-4">Login</h2>
+    <div className="max-w-md mx-auto mt-10 space-y-5">
+      <h2 className="text-2xl font-bold">Login</h2>
 
       <form onSubmit={handleSubmit} className="space-y-3">
         <input
@@ -26,16 +38,25 @@ export default function Login() {
           type="email"
           onChange={(e) => setForm({ ...form, email: e.target.value })}
         />
+
         <input
           className="w-full border p-2"
           placeholder="Password"
           type="password"
           onChange={(e) => setForm({ ...form, password: e.target.value })}
         />
+
         <button className="bg-black text-white w-full py-2">
           Login
         </button>
       </form>
+
+      <div className="text-center text-gray-500">OR</div>
+
+      <GoogleLogin
+        onSuccess={handleGoogleLogin}
+        onError={() => console.log("Google Login Failed")}
+      />
     </div>
   )
 }
