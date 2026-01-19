a83cb520f0b987931c561d20cddde8dd1bdf1a7b
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-01-19 19:38:11 +0530

    Advanced Filters & Pagination

--- health-symptom-tracker/backend/src/controllers/symptom.controller.ts
+++ health-symptom-tracker/backend/src/controllers/symptom.controller.ts
@@ -15,11 +15,45 @@ export const addSymptom = async (req: AuthRequest, res: Response) => {
 
   res.status(201).json(symptom)
 }
-
 export const getSymptoms = async (req: AuthRequest, res: Response) => {
-  const symptoms = await Symptom.find({ user: req.user._id }).sort({
-    date: -1,
-  })
+  const {
+    from,
+    to,
+    severity,
+    page = "1",
+    limit = "5",
+  } = req.query
 
-  res.json(symptoms)
+  const query: any = {
+    user: req.user._id,
+  }
+
+  if (from && to) {
+    query.date = {
+      $gte: new Date(from as string),
+      $lte: new Date(to as string),
+    }
+  }
+
+  if (severity) {
+    query.severity = Number(severity)
+  }
+
+  const pageNumber = Number(page)
+  const limitNumber = Number(limit)
+  const skip = (pageNumber - 1) * limitNumber
+
+  const symptoms = await Symptom.find(query)
+    .sort({ date: -1 }) 
+    .skip(skip)
+    .limit(limitNumber)
+
+  const total = await Symptom.countDocuments(query)
+  res.json({
+    total,
+    page: pageNumber,
+    pages: Math.ceil(total / limitNumber),
+    data: symptoms,
+  })
 }
+
